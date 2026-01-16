732f2dfc103411a6e2a1f8fc26c16fc036c6429d
vinii-pimentel <vinii_2302@hotmail.com>
2026-01-15 22:08:08 -0300

    Deploy no Render do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,17 @@ import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';//Para executar Remotamente (Render)
+import { DevService } from './data/services/dev.service';//Para executar localmente
 
 // Decorator - Etiqueta de Metadados
 @Module({
   imports: [  // Configurando o TypeORM
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+        imports: [ConfigModule],
     }),
     PostagemModule,
     TemaModule,
